1cb45b43f9264fcd97b7368686ef67f59348772c
iamshaylajade <shaylajade21@yahoo.com>
2023-12-21 23:32:58 -0500

    updating server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
