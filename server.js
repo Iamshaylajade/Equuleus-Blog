74965032a05707b6a98adba51f534dacb938ca2a
iamshaylajade <shaylajade21@yahoo.com>
2023-12-21 23:37:03 -0500

    updates to js

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
